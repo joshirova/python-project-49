2a3f462596eff581e1a1dda88c9efad29082a990
Юлия Оширова <weiwei04@mail.ru>
2024-12-06 20:34:25 +0300

    Added Makefile, package build, shebang commands to brain_games.py

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# brain_games.py
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+
+def main():
+    welcome_user()
+
+if __name__ == "__main__":
+    main()
+
